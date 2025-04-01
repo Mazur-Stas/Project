cbf87437a5c66d9b82b4132ad7c699b8fb4310f0
Mazur-Stas <mazur.stas5400@gmail.com>
2025-04-01 21:21:48 +0300

    create table, insert value

---
+++ Project.DAL/Enteties/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Project.DAL.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
+
---
+++ Project.DAL/Migrations/20250401173455_IntoValue.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntoValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            INSERT INTO Students (Name,Description)
+            VALUES ('Maks','Test'),
+                   ('Ivan','Test Ivan'),
+                   ('Vlad','Test Vlad'),
+                   ('Andriy','Test Andriy');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
