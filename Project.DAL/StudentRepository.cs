b466b5f9d8c4b0ad3625ffc4052ec9c7fc65f35d
Mazur-Stas <mazur.stas5400@gmail.com>
2025-04-12 10:39:59 +0300

    Add service , repository for student

---
+++ Project.DAL/StudentRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Project.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL
+{
+    public class StudentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public StudentRepository()
+        {
+            _context = new AppDbContext();
+        }
+
+        public  IEnumerable<Student> GetAll()
+        {
+            return _context.Students;
+        }
+
+
+
+        public void Add (Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+        }
+        public void AddList(List<Student> students)
+        {
+            _context.Students.AddRange(students);
+            _context.SaveChanges();
+
+        }
+
+
+        public void Delete(Student student)
+        {
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+        }
+        public void Update(Student student)
+        {
+            _context.Students.Update(student);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Project/StudentSevice.cs
@@ -0,0 +1,92 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Project.DAL;
+using Project.DAL.Entities;
+
+namespace Project
+{
+    public class StudentSevice
+    {
+        private StudentRepository _studentRepository;
+        public StudentSevice()
+        {
+            _studentRepository = new StudentRepository();
+        }
+
+        public List<Student> GetAll()
+        {
+            return _studentRepository.GetAll().ToList();
+        }
+
+        public List<Student> GetAllById(int id)
+        {
+            return _studentRepository.GetAll()
+                                     .Where(s => s.Id == id)
+                                     .ToList();
+        }
+        public List<Student> GetAllByName(string name)
+        {
+            return _studentRepository.GetAll()
+                                     .Where(s => s.Name == name)
+                                     .ToList();
+        }
+        public Student? GetByName(string name)
+        {
+            return _studentRepository.GetAll().FirstOrDefault(s => s.Name == name);
+        }
+
+        public void Add(Student student)
+        {
+            _studentRepository.Add(student);
+
+        }
+        public void AddList(List<Student> students)
+        {
+            _studentRepository.AddList(students);
+
+        }
+        public void Delete(Student student)
+        {
+            _studentRepository.Delete(student);
+           
+        }
+        public void DeleteById(int id)
+        {
+            var student = _studentRepository.GetAll().FirstOrDefault(s => s.Id == id);
+            if (student != null)
+            {
+                _studentRepository.Delete(student);
+            }
+        }
+        public void DeleteByName(string name)
+        {
+            var student = _studentRepository.GetAll().FirstOrDefault(s => s.Name == name);
+            if (student != null)
+            {
+                _studentRepository.Delete(student);
+            }
+        }
+        public void Update(Student student)
+        {
+            _studentRepository.Update(student);
+            
+        }
+        public void UpdateById(int id)
+        {
+            var student = _studentRepository.GetAll().FirstOrDefault(s => s.Id == id);
+            if (student != null)
+            {
+                _studentRepository.Update(student);
+            }
+        }
+        public void UpdateByName(string name)
+        {
+            var student = _studentRepository.GetAll().FirstOrDefault(s => s.Name == name);
+            if (student != null)
+            {
+                student.Name = name;
+                _studentRepository.Update(student);
+            }
+        }
+    }
+}
