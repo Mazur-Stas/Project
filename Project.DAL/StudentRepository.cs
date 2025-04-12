07a2ef1d03a59e5a7b6b904c5f5f4e40c66c25d4
Mazur-Stas <mazur.stas5400@gmail.com>
2025-04-12 11:07:36 +0300

    Update menu service

--- Project.DAL/StudentRepository.cs
+++ Project.DAL/StudentRepository.cs
@@ -1,51 +1,52 @@
-﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+﻿using Project.DAL;
 using Project.DAL.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Project.DAL
+using Project;
+
+namespace ConsoleApp1.DAL.Repositories
 {
     public class StudentRepository
     {
         private readonly AppDbContext _context;
-
         public StudentRepository()
         {
             _context = new AppDbContext();
         }
-
-        public  IEnumerable<Student> GetAll()
+        public IEnumerable<Student> GetAllStudents()
         {
             return _context.Students;
         }
 
+        public Student? GetStudentById(int id)
+        {
+            return _context.Students.FirstOrDefault(s => s.Id == id);
 
+        }
 
-        public void Add (Student student)
+        public Student? GetStudentByName(string name)
+        {
+            return _context.Students.FirstOrDefault(s => s.Name == name);
+        }
+
+        public void AddStudent(Student student)
         {
             _context.Students.Add(student);
             _context.SaveChanges();
-
         }
-        public void AddList(List<Student> students)
+        public void AddStudents(List<Student> students)
         {
             _context.Students.AddRange(students);
             _context.SaveChanges();
-
         }
 
-
-        public void Delete(Student student)
-        {
-            _context.Students.Remove(student);
-            _context.SaveChanges();
-        }
-        public void Update(Student student)
+        public void UpdateStudent(Student student)
         {
             _context.Students.Update(student);
+            _context.SaveChanges();
+        }
+        public void DeleteStudent(Student student)
+        {
+            _context.Students.Remove(student);
             _context.SaveChanges();
         }
     }
